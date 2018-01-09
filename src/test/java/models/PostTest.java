ed0a7a91d1b9f45473c0be5fcd9ef9d069a1fdc8
James Donlan and Paul Kotok <paul@>
2018-01-09 13:21:03 -0800

    blog lessons continued, added ids

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,5 +1,5 @@
 package models;
-
+import java.time.LocalDateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,33 +7,70 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by Guest on 1/9/18.
+ * Created by Guest on 1/8/18.
  */
 public class PostTest {
-
     @Before
     public void setUp() throws Exception {
-
-
     }
 
     @After
     public void tearDown() throws Exception {
-        Post.clearAllPosts(); //clear out allll the posts before each test.
+        Post.clearAllPosts();
     }
-
     @Test
     public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+    }
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+    @Test
+    public void AllPostsContainsAllPosts_true() {
         Post post = new Post("Day 1: Intro");
         Post otherPost = new Post ("How to pair successfully");
         assertTrue(Post.getAll().contains(post));
         assertTrue(Post.getAll().contains(otherPost));
     }
     @Test
-    public void PostInstantiatesWithContent_true() throws Exception {
-        Post post = new Post("Day 1: Intro");
-        assertEquals("Day 1: Intro", post.getContent());
+    public void getPublished_isFalseAfterInstantiation_false() throws Exception {
+        Post myPost = new Post("Day 1: Intro");
+        assertEquals(false, myPost.getPublished());
+    }
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception{
+        Post myPost = setupNewPost(); //see below
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myPost.getCreatedAt().getDayOfWeek());
+    }
+    @Test
+    public void getId_postsInstantiateWithAnID_1() throws Exception{
+        Post.clearAllPosts();  // Remember, the test will fail without this line! We need to empty leftover Posts from previous tests!
+        Post myPost = new Post("Day 1: Intro");
+        assertEquals(1, myPost.getId());
+    }
+    @Test
+    public void findReturnsCorrectPost() throws Exception {
+        Post post = setupNewPost();
+        assertEquals(1, Post.findById(post.getId()).getId());
+    }
+    @Test
+    public void findReturnsCorrectPostWhenMoreThanOnePostExists() throws Exception {
+        Post post = setupNewPost();
+        Post otherPost = new Post("How to pair successfully");
+        assertEquals(2, Post.findById(otherPost.getId()).getId());
+    }
 
+    public Post setupNewPost(){
+        return new Post("Day 1: Intro");
     }
 
 }
