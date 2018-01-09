995f1293c0934d9ccdcdc88d822eafd31ba00445
James Donlan and Paul Kotok <james@>
2018-01-09 14:53:16 -0800

    lessons done today

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,11 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/posts/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newpost-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
     get("/", (request, response) -> {
         Map<String, Object> model = new HashMap<>();
         ArrayList<Post> posts = Post.getAll();
@@ -23,5 +28,30 @@ public class App {
         Post newPost = new Post(content);
         return new ModelAndView(model, "success.hbs");
     }, new HandlebarsTemplateEngine());
+
+    post("/posts/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+
+        String content = request.queryParams("content");
+        Post newPost = new Post(content);
+        model.put("post", newPost);
+        return new ModelAndView(model, "success.hbs");
+    }, new HandlebarsTemplateEngine());
+
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        ArrayList<Post> posts = Post.getAll();
+        model.put("posts", posts);
+
+        return new ModelAndView(model, "index.hbs");
+    }, new HandlebarsTemplateEngine());
+
+        get("/posts/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Post foundPost = Post.findById(idOfPostToFind); //use it to find post
+            model.put("post", foundPost); //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
 }
 }
